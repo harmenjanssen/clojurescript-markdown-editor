5819607c5f9c91abe76deb75b0f68ae87f1a5ab5
Harmen Janssen <harmen@grrr.nl>
2020-04-26 09:44:29 +0200

    Finish lesson 4: Add copy buttons

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -8,6 +8,22 @@
 (defn md->html [md]
   (.makeHtml showdown-converter md))
 
+(defn copy-to-clipboard [s]
+  (let [el (.createElement js/document "textarea")
+        selected (when (pos? (-> js/document .getSelection .-rangeCount))
+                   (-> js/document .getSelection (.getRangeAt 0)))]
+    (set! (.-value el) s)
+    (.setAttribute el "readonly" "")
+    (set! (-> el .-style .-position) "absolute")
+    (set! (-> el .-style .-left) "-9999px")
+    (-> js/document .-body (.appendChild el))
+    (.select el)
+    (.execCommand js/document "copy")
+    (-> js/document .-body (.removeChild el))
+    (when selected
+      (-> js/document .getSelection .removeAllRanges)
+      (-> js/document .getSelection (.addRange selected)))))
+
 (defn app []
   [:div
     [:h1 "Markdownify"]
@@ -17,17 +33,31 @@
         {:style {:flex "1"}}
         [:h2 "Markdown"]
         [:textarea
-        {:on-change #(reset! markdown (-> % .-target .-value))
-          :value @markdown
-          :style {:resize "none"
-                  :height "500px"
-                  :width "100%"}}]]
+          {:on-change #(reset! markdown (-> % .-target .-value))
+            :value @markdown
+            :style {:resize "none"
+                    :height "500px"
+                    :width "100%"}}]
+        [:button
+         {:on-click #(copy-to-clipboard @markdown)
+          :style {:background-color :green
+                  :padding "1em"
+                  :color :white
+                  :border-radius 10}}
+         "Copy markdown"]]
 
       [:div
         {:style {:flex "1" :padding-left "2em"}}
         [:h2 "HTML Preview"]
         [:div {:style {:height "500px"}
-               :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]]
+               :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
+        [:button
+         {:on-click #(copy-to-clipboard (md->html @markdown))
+          :style {:background-color :green
+                  :padding "1em"
+                  :color :white
+                  :border-radius 10}}
+         "Copy HTML"]]
     ]])
 
 (defn mount! []
